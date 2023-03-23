60af347dd96cad8355569e825ca9d6e296fd3217
NVA <vieetanhnguyeenx@gmail.com>
2023-03-23 11:18:35 +0700

    Fist Setup

---
+++ SupermarketManagmentSystem/SupermarketManagmentSystem/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SupermarketManagmentSystem.Models;
+
+public partial class Product
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public int? Quantity { get; set; }
+
+    public double? Price { get; set; }
+
+    public int? CategoryId { get; set; }
+
+    public virtual Category? Category { get; set; }
+}
---
+++ SupermarketManagmentSystem/SupermarketManagmentSystem/Models/SupermarketManagementSystemContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace SupermarketManagmentSystem.Models;
+
+public partial class SupermarketManagementSystemContext : DbContext
+{
+    public SupermarketManagementSystemContext()
+    {
+    }
+
+    public SupermarketManagementSystemContext(DbContextOptions<SupermarketManagementSystemContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetConnectionString("MyConstr"));
+    }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.ToTable("Category");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Name).HasMaxLength(150);
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.ToTable("Product");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
+            entity.Property(e => e.Name).HasMaxLength(150);
+
+            entity.HasOne(d => d.Category).WithMany(p => p.Products)
+                .HasForeignKey(d => d.CategoryId)
+                .HasConstraintName("FK_Product_Category");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
